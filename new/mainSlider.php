60b6189e74d2606c3b8d74ee3eb1aa516e0aae8b
alecat88 <alecat88@gmail.com>
2017-12-20 15:01:50 +0000

    upload optimized image

--- new/mainSlider.php
+++ new/mainSlider.php
@@ -2,13 +2,13 @@
 	  <div class="columns">
 	  	<div class="slider-wrapper">
   			<div class="slider">
-				<div class="slider--item" style="background-image: url(assets/img/shutterstock_145084777.jpg)">
+				<div class="slider--item" style="background-image: url(assets/img/shutterstock_145084777_opt.jpg)">
 				</div>
-				<div class="slider--item" style="background-image: url(assets/img/shutterstock_533153143.jpg)">ciaooo
+				<div class="slider--item" style="background-image: url(assets/img/shutterstock_533153143_opt.jpg)">ciaooo
 				</div>
-				<div class="slider--item" style="background-image: url(assets/img/shutterstock_536582824.jpg); background-position-y: 100%">
+				<div class="slider--item" style="background-image: url(assets/img/shutterstock_536582824_opt.jpg); background-position-y: 100%">
 				</div>
-				<div class="slider--item" style="background-image: url(assets/img/shutterstock_661602724.jpg)">
+				<div class="slider--item" style="background-image: url(assets/img/shutterstock_661602724_opt.jpg)">
 				</div>
   			</div>
 		</div>
